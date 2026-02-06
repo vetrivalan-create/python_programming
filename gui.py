405aceff8086c3612ac5e0bd8a122b73eacface8
vetrivalan-create <vetrivalan@aiat.edu.in>
2026-02-06 12:01:32 +0530

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,6 +1,15 @@
 import tkinter
+from tkinter import messagebox
 root = tkinter.Tk()
+
+
+def Onclick_Submit():
+    name=name_textbook.get()
+    messagebox.showinfo("Captured Data",name)
+
+
 root.geometry("300x400")
+root.configure(bg="#ADD8E6")
 
 
 root.title("Student Registration Form")
@@ -10,7 +19,7 @@ name_label.pack(anchor=tkinter.W,padx=10)
 name_textbook = tkinter.Entry(root)
 name_textbook.pack(anchor=tkinter.W,padx=10)
 
-email_label = tkinter.Label(root,text="Enter your Email",bg="black",fg="white")
+email_label = tkinter.Label(root,text="Enter your Email")
 email_label.pack(anchor=tkinter.W,padx=10)
 
 email_textbook = tkinter.Entry(root)
@@ -22,7 +31,7 @@ phone_label.pack(anchor=tkinter.W,padx=10)
 phone_textbook = tkinter.Entry(root)
 phone_textbook.pack(anchor=tkinter.W,padx=10)
 
-submit_button = tkinter.Button(root,text = "Submit")
+submit_button = tkinter.Button(root,text = "Submit",command=Onclick_Submit)
 submit_button.pack(anchor=tkinter.W,padx=10)
 
 root.mainloop()
